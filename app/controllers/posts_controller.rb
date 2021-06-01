7e946e31e972c17f8d912764e774e54bc6feaee7
Christopher Arbelo <christopherarbelo@gmail.com>
2021-06-01 13:18:52 -0400

    Implement all actions for Post and form to create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,30 +2,52 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @posts = Post.all.includes(:user)
+    @posts = Post.all.includes(:user).order(created_at: :desc)
+    @post = current_user.posts.build
   end
 
   def show
-    
+    @post = Post.includes(:user).find(params[:id])
   end
 
   def new
-    
+    @post = current_user.posts.build
   end
 
   def create
-    
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
-    
+    @post = current_user.posts.find(params[:id])
   end
 
   def update
-    
+    @post = current_user.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
 
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
