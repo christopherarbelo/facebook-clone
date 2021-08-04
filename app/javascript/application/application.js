7c49907ab2f6281d27cfffb5c2862be6da454537
Christopher Arbelo <christopherarbelo@gmail.com>
2021-08-03 22:08:35 -0400

    Transition to Bootstrap and navbar changes
    A transition was made to bootstrap and navbar styles were changed to take advantage of bootstrap styles.

--- app/javascript/application/application.js
+++
@@ -1,6 +0,0 @@
-const notificationLink = document.querySelector('#notifications-link')
-const notificationPanel = document.querySelector('#notification-panel')
-
-notificationLink.addEventListener('click', e => {
-  notificationPanel.classList.toggle("invisible");
-})
--- app/javascript/packs/custom-application.js
+++
@@ -1,1 +0,0 @@
-import "../application/application.js"
